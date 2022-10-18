d2f9713e1f35b89bcde53f33471be00555ed7c33
swapnilbawane <swapnilbawane08@gmail.com>
2022-10-18 19:26:10 +0530

    add: added source code for app.js file

---
+++ app.js
@@ -0,0 +1,44 @@
+var angleInput = document.querySelectorAll(".angle"); 
+
+var btnCalculate = document.querySelector("#btn-submit"); 
+
+var hiddenMsg = document.querySelector(".hiddenMessage"); 
+
+var indexLinkDeco = document.querySelector("#indexLink"); 
+
+function isTriangle() { 
+
+    var sum = Number(angleInput[0].value)+Number(angleInput[1].value)+Number(angleInput[2].value); 
+
+if(sum===180) 
+{
+hiddenMsg.style.display = "block";
+hiddenMsg.innerText = "Sum of angles is "+sum+". This forms a triangle."; 
+} 
+
+else 
+{
+    hiddenMsg.style.display = "block";
+    hiddenMsg.innerText = "Sorry this does not form a triangle. Enter different values to find out.";   
+}
+
+}
+
+
+indexLinkDeco.style.textDecoration = "underline"; 
+indexLinkDeco.style.color = "white";
+btnCalculate.addEventListener("click",isTriangle); 
+
+
+// TESTS: 
+
+function testInputValuesFirstElement() {
+    // var angleNew = document.querySelector("#angleNew");
+    console.log(btnCalculate);
+    console.log(angleInput[0]);
+    console.log(angleInput[0].value);
+    console.log(typeof (angleNew)); 
+    console.log(angleNew); 
+    console.log(angleNew.value);
+    console.log(typeof(angleInput[0].value));
+}
