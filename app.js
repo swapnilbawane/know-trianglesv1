9620ae0de8a29004c864c41bd7eefccad1c38a01
Swapnil Bawane <swapnilbawane08@gmail.com>
2022-12-01 16:09:47 +0530

    update logic for edge cases.

--- app.js
+++ app.js
@@ -1,55 +1,45 @@
-var angleInput = document.querySelectorAll(".angle"); 
+var angleInput = document.querySelectorAll(".angle");
 
-var btnCalculate = document.querySelector("#btn-submit"); 
+var btnCalculate = document.querySelector("#btn-submit");
 
-var hiddenMsg = document.querySelector(".hiddenMessage"); 
+var hiddenMsg = document.querySelector(".hiddenMessage");
 
-var indexLinkDeco = document.querySelector("#indexLink"); 
+var indexLinkDeco = document.querySelector("#indexLink");
 
-function isTriangle() { 
+function isTriangle() {
+  if (Number(angleInput[0].value) > 0 && Number(angleInput[1].value) > 0 && Number(angleInput[2].value) > 0) {
+    var sum = Number(angleInput[0].value) + Number(angleInput[1].value) + Number(angleInput[2].value);
 
-    if(angleInput[0].value && angleInput[1].value && angleInput[2].value)
-    {
-        
-     if (angleInput[0].value > 0 && angleInput[1].value > 0 && angleInput[2].value > 0 ) 
-     {
-         var sum = Number(angleInput[0].value)+Number(angleInput[1].value)+Number(angleInput[2].value);
-         
-         if(sum===180) 
-     {
+    if (sum === 180) {
       hiddenMsg.style.display = "block";
-      hiddenMsg.innerText = "Sum of angles is "+sum+". This forms a triangle."; 
-     } 
-      
-         else 
-    {
-    hiddenMsg.style.display = "block";
-    hiddenMsg.innerText = "Sorry this does not form a triangle. Enter different values to find out.";   
+      hiddenMsg.innerText = "Sum of angles is " + sum + ". This forms a triangle.";
     }
-         
-     }
-     else hiddenMsg.innerText = "Please enter value greater than 0.";     
-    } 
-    
-    else hiddenMsg.innerText = "Sorry please enter all values.";
-   
+
+    else {
+      hiddenMsg.style.display = "block";
+      hiddenMsg.innerText = "Sorry this does not form a triangle. Enter different values to find out.";
+    }
+
+  }
+  else { hiddenMsg.innerText = "Invalid/Incorrect Input/ Please enter value greater than 0 only."; }
+
 }
 
 
-indexLinkDeco.style.textDecoration = "underline"; 
+indexLinkDeco.style.textDecoration = "underline";
 indexLinkDeco.style.color = "white";
-btnCalculate.addEventListener("click",isTriangle); 
+btnCalculate.addEventListener("click", isTriangle);
 
 
 // TESTS: 
 
 function testInputValuesFirstElement() {
-    // var angleNew = document.querySelector("#angleNew");
-    console.log(btnCalculate);
-    console.log(angleInput[0]);
-    console.log(angleInput[0].value);
-    console.log(typeof (angleNew)); 
-    console.log(angleNew); 
-    console.log(angleNew.value);
-    console.log(typeof(angleInput[0].value));
+  
+  console.log(btnCalculate);
+  console.log(angleInput[0]);
+  console.log(angleInput[0].value);
+  console.log(angleInput[1].value);
+  console.log(angleInput[2].value);
+  console.log(typeof (angleInput[0].value));
+  console.log("str" + ((Number(angleInput[0].value) > 0) && (Number(angleInput[1].value)) > 0 && (Number(angleInput[2].value) > 0)));
 }
